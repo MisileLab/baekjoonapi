fa169e3838d6b96703867ebed0f0412ad2a7017a
misilelaboratory <misileminecord@gmail.com>
2022-11-16 22:17:48 +0900

    no suffix

--- bojapi/__init__.py
+++ bojapi/__init__.py
@@ -6,7 +6,7 @@ class BaekjoonUser:
     def __init__(self, user_name):
         soup = BeautifulSoup(get('https://www.acmicpc.net/user/' + user_name).text, 'lxml')
         self.user_name = user_name
-        self.status = soup.find('blockquote', {'class': 'no-mathjax'}).text
+        self.status = soup.find('blockquote', {'class': 'no-mathjax'}).text.removesuffix('정보언어')
         self.rank = soup.select_one("#statics > tbody > tr:nth-child(1) > td").text
         self.correct_qs = soup.select_one("body > div.wrapper > div.container.content > div.row > div:nth-child(2) > div > div.col-md-9 > div:nth-child(1) > div.panel-body").text
         self.correct_q = soup.select_one("#u-solved").text
